2beee955d7a5ffa38eb59f2c982317585404c3c4
Annbekky <anya.love@inbox.ru>
2026-03-20 15:49:06 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,25 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
 
-// Замените my-figma-project на точное имя вашего репозитория
-const repoName = 'Figma'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  base: `/${repoName}/`,
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  base: '/Figma/', // имя репозитория на GitHub
+  plugins: [react()],
   resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-  assetsInclude: ['**/*.svg', '**/*.csv'],
-})
-  
+  build: {
+    target: 'esnext',
+    outDir: 'dist',
+    emptyOutDir: true, // очищать dist перед сборкой
+  },
   server: {
     port: 3000,
     open: true,
